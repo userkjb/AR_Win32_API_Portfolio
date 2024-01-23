2365395b6c1cdf5237d20cbe653f9aeaad6bb72b
KJB <kjb_jh@naver.com>
2024-01-23 14:55:00 +0900

    Fixed typo.
    # 오타 수정.

--- Mega_man_X4_WinAPI/EngineBase/EngineDebug.h
+++ Mega_man_X4_WinAPI/EngineBase/EngineDebug.h
@@ -2,7 +2,7 @@
 #include <Windows.h>
 #include <assert.h>
 
-namespace engineDebug
+namespace EngineDebug
 {
 	#define LeakCheck _CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
 	#define MsgBoxAssert(TEXT) MessageBoxA(nullptr, TEXT, "치명적 에러", MB_OK); assert(false);
