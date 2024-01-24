5d6e98c82bec9ef9bb4a37d1d7fad5d936642672
KJB <kjb_jh@naver.com>
2024-01-24 22:39:49 +0900

    Add std::string ToUpper Function
    # std::string 데이터를 대문자로 바꿔 주는 함수 작성.

--- Mega_man_X4_WinAPI/EngineBase/EngineString.h
+++ Mega_man_X4_WinAPI/EngineBase/EngineString.h
@@ -1,20 +1,23 @@
 #pragma once
+#include <string>
+#include <string_view>
+
 class EngineString
 {
 public :
+	static std::string ToUpper(std::string_view View);
+
+protected :
+
+private :
 	// constructer destructer
 	EngineString();
-	virtual ~EngineString();
+	~EngineString();
 
 	// delete Function
 	EngineString(const EngineString& _Other) = delete;
 	EngineString(EngineString&& _Other) noexcept = delete;
 	EngineString& operator=(const EngineString) = delete;
 	EngineString& operator=(EngineString&& _Other) noexcept = delete;
-
-protected :
-
-private :
-
 };
 
