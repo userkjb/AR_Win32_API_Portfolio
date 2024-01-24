beaf06a3444496b83c0c111b76b22d7aca76c0cf
KJB <kjb_jh@naver.com>
2024-01-24 23:38:34 +0900

    Write two ways to start the game.
    # 게임 시작 방법 2가지를 적음
    # 결과는 똑같음. - 메크로를 사용하냐 안하냐 차이일 뿐이다.

--- Mega_man_X4_WinAPI/MegaManX4/MegaManX4.cpp
+++ Mega_man_X4_WinAPI/MegaManX4/MegaManX4.cpp
@@ -3,4 +3,14 @@
 #include <EnginePlatform\EngineWindow.h>
 #include <Contents\ContentsCore.h>
 
-ENGINESTART(ContentsCore)
+//ENGINESTART(ContentsCore)
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	LeakCheck;
+	ContentsCore NewUserCore = ContentsCore();
+	EngineCore::EngineStart(hInstance, &NewUserCore);
+}
