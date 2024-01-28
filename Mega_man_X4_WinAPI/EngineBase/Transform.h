76014b52ed212e12f94ef4cd7b63a40d1112de2d
KJB <kjb_jh@naver.com>
2024-01-28 22:16:53 +0900

    Up, down, left and right values ​​return. This value is returned by converting to int type rather than float type.
    # 상하좌우 값 return.
    # float형이 아닌 int형으로 형변환해서 이 값을 return 함.

--- Mega_man_X4_WinAPI/EngineBase/Transform.h
+++ Mega_man_X4_WinAPI/EngineBase/Transform.h
@@ -28,6 +28,40 @@ public :
 		return Position;
 	}
 
+	float Left()
+	{
+		return Position.X - Scale.hX();
+	}
+	float Right()
+	{
+		return Position.X + Scale.hX();
+	}
+	float Top()
+	{
+		return Position.Y - Scale.hY();
+	}
+	float Bottom()
+	{
+		return Position.Y + Scale.hY();
+	}
+
+	int iLeft()
+	{
+		return static_cast<int>(Left());
+	}
+	int iRight()
+	{
+		return static_cast<int>(Right());
+	}
+	int iTop()
+	{
+		return static_cast<int>(Top());
+	}
+	int iBottom()
+	{
+		return static_cast<int>(Bottom());
+	}
+
 private :
 	FVector Scale;
 	FVector Position;
