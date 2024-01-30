602a1a6d9f928a69dccd8bd0aba2ec861ced0647
KJB <kjb_jh@naver.com>
2024-01-30 19:37:52 +0900

    Implementation of functions to be used externally.
    # 외부에서 사용할 함수들 기능 구현.

--- Mega_man_X4_WinAPI/EnginePlatform/EngineInput.h
+++ Mega_man_X4_WinAPI/EnginePlatform/EngineInput.h
@@ -2,6 +2,7 @@
 
 #include <map>
 #include <Windows.h>
+#include <EngineBase\EngineDebug.h>
 
 class EngineInput
 {
@@ -40,10 +41,42 @@ public :
 	EngineInput& operator=(EngineInput&& _Other) noexcept = delete;
 
 	// 외부에서 사용할 함수들.
-	static bool IsDown(int _Key);
-	static bool IsPress(int _Key);
-	static bool IsUp(int _Key);
-	static bool IsFree(int _Key);
+	static bool IsDown(int _Key)
+	{
+		if (AllKeys.contains(_Key) == false)
+		{
+			MsgBoxAssert("입력설정이 존재하지 않는 키 입니다");
+		}
+
+		return AllKeys[_Key].Down;
+	}
+	static bool IsPress(int _Key)
+	{
+		if (false == AllKeys.contains(_Key))
+		{
+			MsgBoxAssert("입력설정이 존재하지 않는 키 입니다");
+		}
+
+		return AllKeys[_Key].Press;
+	}
+	static bool IsUp(int _Key)
+	{
+		if (false == AllKeys.contains(_Key))
+		{
+			MsgBoxAssert("입력설정이 존재하지 않는 키 입니다");
+		}
+
+		return AllKeys[_Key].Up;
+	}
+	static bool IsFree(int _Key)
+	{
+		if (false == AllKeys.contains(_Key))
+		{
+			MsgBoxAssert("입력설정이 존재하지 않는 키 입니다");
+		}
+
+		return AllKeys[_Key].Free;
+	}
 
 	static void KeyCheckTick(float _DeltaTime);
 protected :
