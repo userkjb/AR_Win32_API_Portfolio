c9194b5942da5a7bd0173a44788b81480d5c6c01
KJB <kjb_jh@naver.com>
2024-01-30 18:55:05 +0900

    Function and map declarations
    # 함수 및 map 선언

--- Mega_man_X4_WinAPI/EnginePlatform/EngineInput.h
+++ Mega_man_X4_WinAPI/EnginePlatform/EngineInput.h
@@ -1,4 +1,7 @@
 #pragma once
+
+#include <map>
+
 class EngineInput
 {
 
@@ -16,7 +19,7 @@ private:
 		bool Up = false; // 떼어진 순간
 		bool Free = true; // 누리지 않으면
 
-		int Key = -1;
+		int Key = -1; // 기본 키 값.
 
 		void KeyCheck();
 	};
@@ -33,9 +36,14 @@ public :
 	EngineInput& operator=(const EngineInput) = delete;
 	EngineInput& operator=(EngineInput&& _Other) noexcept = delete;
 
+	static bool IsDown(int _Key);
+	static bool IsPress(int _Key);
+	static bool IsUp(int _Key);
+	static bool IsFree(int _Key);
+
 	static void KeyCheckTick(float _DeltaTime);
 protected :
-
+	static std::map<int, EngineKey> AllKeys;
 private :
 	static void InputInit();
 };
