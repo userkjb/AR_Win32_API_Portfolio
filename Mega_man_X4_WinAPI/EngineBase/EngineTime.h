64ce3a6f2cb9dba703d3315ad5bfdbbc29c1ca73
KJB <kjb_jh@naver.com>
2024-01-30 23:20:57 +0900

    Add EngineTime Class
    #  DeltaTime을 위한 Time Class 추가.

---
+++ Mega_man_X4_WinAPI/EngineBase/EngineTime.h
@@ -0,0 +1,20 @@
+#pragma once
+class EngineTime
+{
+public :
+	// constrcuter destructer
+	EngineTime();
+	~EngineTime();
+
+	// delete Function
+	EngineTime(const EngineTime& _Other) = delete;
+	EngineTime(EngineTime&& _Other) noexcept = delete;
+	EngineTime& operator=(const EngineTime& _Other) = delete;
+	EngineTime& operator=(EngineTime&& _Other) noexcept = delete;
+
+protected :
+
+private :
+
+};
+
