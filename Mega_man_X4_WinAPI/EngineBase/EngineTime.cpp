659295f60b245d9341ccf360efdda3b9845e7142
KJB <kjb_jh@naver.com>
2024-02-01 21:24:42 +0900

    DeltaTime functions implemented.
    # DeltaTime 함수들 구현 완료.

--- Mega_man_X4_WinAPI/EngineBase/EngineTime.cpp
+++ Mega_man_X4_WinAPI/EngineBase/EngineTime.cpp
@@ -2,6 +2,7 @@
 
 EngineTime::EngineTime()
 {
+	TimeCheckStart();
 }
 
 EngineTime::~EngineTime()
@@ -10,9 +11,22 @@ EngineTime::~EngineTime()
 
 void EngineTime::TimeCheckStart()
 {
+	// 초당 샐 수 있는 값
+	QueryPerformanceFrequency(&Count);
+	dCount = static_cast<double>(Count.QuadPart);
+
+	// 호출 기준 현재 까지 샌 값
+	QueryPerformanceCounter(&PrevTime);
 }
 
 float EngineTime::TimeCheck()
 {
-	return 0.0f;
+	QueryPerformanceCounter(&CurTime);
+	__int64 Tick = (CurTime.QuadPart - PrevTime.QuadPart);
+
+	// 정밀도를 높이기 위해 float이 아닌 double로 계산.
+	double dTick = static_cast<double>(Tick);
+	DeltaTime = dTick / dCount;
+	PrevTime.QuadPart = CurTime.QuadPart;
+	return static_cast<float>(DeltaTime);
 }
