f1f383011e9b37ce42673568b795cc668544a5aa
KJB <kjb_jh@naver.com>
2024-02-01 21:18:34 +0900

    Added comment for LARGE_INTEGER.
    # LARGE_INTEGER에 대한 주석 추가.

--- Mega_man_X4_WinAPI/EngineBase/EngineTime.h
+++ Mega_man_X4_WinAPI/EngineBase/EngineTime.h
@@ -35,3 +35,14 @@ private :
 	double DeltaTime;
 };
 
+//typedef union _LARGE_INTEGER {
+//    struct {
+//        DWORD LowPart;
+//        LONG HighPart;
+//    } DUMMYSTRUCTNAME;
+//    struct {
+//        DWORD LowPart;
+//        LONG HighPart;
+//    } u;
+//    LONGLONG QuadPart;
+//} LARGE_INTEGER;
