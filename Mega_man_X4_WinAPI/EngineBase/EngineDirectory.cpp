975cd9c4750b8ada7a0b2d883dc84d62745c69d9
KJB <kjb_jh@naver.com>
2024-02-05 21:20:57 +0900

    Implementation of the AllFileRecursive function function.
    # AllFileRecursive 함수 기능 구현.

--- Mega_man_X4_WinAPI/EngineBase/EngineDirectory.cpp
+++ Mega_man_X4_WinAPI/EngineBase/EngineDirectory.cpp
@@ -33,5 +33,45 @@ std::list<UEngineFile> UEngineDirectory::AllFile(std::vector<std::string> _Ext, 
 
 void UEngineDirectory::AllFileRecursive(const std::string_view _Path, std::list<UEngineFile>& _Result, std::vector<std::string> _Ext, bool _Recursive)
 {
+	std::filesystem::directory_iterator DirIter = std::filesystem::directory_iterator(_Path);
 
+	// 받은 경로의 디렉토리를 순회.
+	// 디렉토리의 정보. - entry
+	for (const std::filesystem::directory_entry& Entry : DirIter)
+	{
+		std::filesystem::path Path = Entry.path();
+		std::filesystem::path Ext = Entry.path().extension();
+		std::string UpperExt = UEngineString::ToUpper(Ext.string());
+
+		if (true == Entry.is_directory())
+		{
+			if (true == _Recursive)
+			{
+				AllFileRecursive(Path.string(), _Result, _Ext, _Recursive);
+			}
+			continue;
+		}
+
+		if (0 == _Ext.size())
+		{
+			_Result.push_back(UEngineFile(Path.string()));
+			continue;
+		}
+
+		
+		bool Check = false;
+
+		for (size_t i = 0; i < _Ext.size(); i++)
+		{
+			if (_Ext[i] == UpperExt)
+			{
+				Check = true;
+			}
+		}
+
+		if (true == Check)
+		{
+			_Result.push_back(UEngineFile(Path.string()));
+		}
+	}
 }
