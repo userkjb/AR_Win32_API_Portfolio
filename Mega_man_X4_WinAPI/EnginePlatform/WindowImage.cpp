6885581ad084dd5085cbe23049041b6e545f317d
KJB <kjb_jh@naver.com>
2024-02-05 22:06:57 +0900

    Add WindowImage Class
    # WindowImage Class 추가.
    # 모든 이미지 처리 DC

---
+++ Mega_man_X4_WinAPI/EnginePlatform/WindowImage.cpp
@@ -0,0 +1,11 @@
+#include "WindowImage.h"
+
+#pragma comment(lib, "Msimg32.lib")
+
+UWindowImage::UWindowImage()
+{
+}
+
+UWindowImage::~UWindowImage()
+{
+}
---
+++ Mega_man_X4_WinAPI/EnginePlatform/WindowImage.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+class UWindowImage
+{
+public:
+	// constrcuter destructer
+	UWindowImage();
+	~UWindowImage();
+
+	// delete Function
+	UWindowImage(const UWindowImage& _Other) = delete;
+	UWindowImage(UWindowImage&& _Other) noexcept = delete;
+	UWindowImage& operator=(const UWindowImage& _Other) = delete;
+	UWindowImage& operator=(UWindowImage&& _Other) noexcept = delete;
+};
+
