6f344f297c01ce97d79e54cde92fbdeebb935ad6
KJB <kjb_jh@naver.com>
2024-02-05 20:39:41 +0900

    EnginePath Class declaration and implementation. Beware of subjunctive functions.
    # EnginePath Class 선언 및 구현.
    # 제귀함수 조심.

--- Mega_man_X4_WinAPI/EngineBase/EnginePath.cpp
+++ Mega_man_X4_WinAPI/EngineBase/EnginePath.cpp
@@ -1,1 +1,68 @@
 #include "EnginePath.h"
+#include "EngineDebug.h"
+
+UEnginePath::UEnginePath()
+	: Path(std::filesystem::current_path())
+{
+}
+
+UEnginePath::UEnginePath(std::filesystem::path _Path)
+	:Path(_Path)
+{
+}
+
+UEnginePath::~UEnginePath()
+{
+}
+
+bool UEnginePath::IsFile()
+{
+	return !std::filesystem::is_directory(Path);
+}
+
+bool UEnginePath::IsDirectory()
+{
+	return std::filesystem::is_directory(Path);
+}
+
+bool UEnginePath::IsExists()
+{
+	return std::filesystem::exists(Path);
+}
+
+void UEnginePath::MoveParent()
+{
+	Path = Path.parent_path();
+}
+
+void UEnginePath::Move(std::string_view _Path)
+{
+	std::filesystem::path NextPath = Path;
+	NextPath.append(_Path);
+
+	bool Check = std::filesystem::exists(NextPath);
+
+	if (false == Check)
+	{
+		MsgBoxAssert(NextPath.string() + "라는 경로는 존재하지 않습니다.");
+	}
+
+	Path = NextPath;
+}
+
+std::string UEnginePath::GetFileName() const
+{
+	std::filesystem::path Text = Path.filename();
+	return Text.string();
+}
+
+std::string UEnginePath::GetExtension() const
+{
+	std::filesystem::path Text = Path.extension();
+	return Text.string();
+}
+
+std::string UEnginePath::GetFullPath() const
+{
+	return Path.string();
+}
--- Mega_man_X4_WinAPI/EngineBase/EnginePath.h
+++ Mega_man_X4_WinAPI/EngineBase/EnginePath.h
@@ -1,6 +1,37 @@
 #pragma once
+#include <string>
+#include <filesystem>
 
 class UEnginePath
 {
+public :
+	UEnginePath();
+	UEnginePath(std::filesystem::path _Path);
+	~UEnginePath();
 
+	bool IsFile();
+	bool IsDirectory();
+	bool IsExists();
+
+	void MoveParent();
+	void Move(std::string_view _Path);
+
+	/// <summary>
+	/// 파일 이름 가져오기.
+	/// </summary>
+	/// <returns></returns>
+	std::string GetFileName() const;
+	/// <summary>
+	/// 파일 확장자 가져오기.
+	/// </summary>
+	/// <returns></returns>
+	std::string GetExtension() const;
+	/// <summary>
+	/// 파일 경로 가져오기
+	/// </summary>
+	/// <returns></returns>
+	std::string GetFullPath() const;
+
+protected :
+	std::filesystem::path Path;
 };
