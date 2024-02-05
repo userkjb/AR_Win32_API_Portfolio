72f19223eae595bcebc19691e66b3eee1c2994f8
KJB <kjb_jh@naver.com>
2024-02-05 20:57:45 +0900

    Declaration of functions that store all files in the directory in a list.
    # Directory에 들어있는 파일들을 전부 List에 담는 함수들 선언.

--- Mega_man_X4_WinAPI/EngineBase/EngineDirectory.cpp
+++ Mega_man_X4_WinAPI/EngineBase/EngineDirectory.cpp
@@ -7,3 +7,14 @@ UEngineDirectory::UEngineDirectory()
 UEngineDirectory::~UEngineDirectory()
 {
 }
+
+std::list<UEngineFile> UEngineDirectory::AllFile(std::vector<std::string> _Ext, bool _Recursive)
+{
+	return std::list<UEngineFile>();
+}
+
+
+void UEngineDirectory::AllFileRecursive(const std::string_view _Path, std::list<UEngineFile>& _Result, std::vector<std::string> _Ext, bool _Recursive)
+{
+
+}
--- Mega_man_X4_WinAPI/EngineBase/EngineDirectory.h
+++ Mega_man_X4_WinAPI/EngineBase/EngineDirectory.h
@@ -1,8 +1,37 @@
 #pragma once
+#include "EnginePath.h"
+#include <list>
 
-class UEngineDirectory
+class UEngineFile;
+
+class UEngineDirectory : public UEnginePath
 {
 public :
 	UEngineDirectory();
 	~UEngineDirectory();
+
+	/// <summary>
+	/// 외부에서 사용할 파일들을 찾는 함수.
+	/// </summary>
+	/// <param name="_Ext">확장자들. 대/소문자 가리지 않음.</param>
+	/// <param name="_Recursive">하위 폴더 까지 더 볼 것인지?</param>
+	/// <returns></returns>
+	std::list<UEngineFile> AllFile(
+		std::vector<std::string> _Ext = std::vector<std::string>(),
+		bool _Recursive = false);
+
+private :
+	/// <summary>
+	/// 클래스 내부에서 진짜로 파일을 찾는 함수.
+	/// 찾아서 List(UEngineFile)에 담음.
+	/// </summary>
+	/// <param name="_Path">경로</param>
+	/// <param name="_Result">경로 담겨 있는 list</param>
+	/// <param name="_Ext">확장자</param>
+	/// <param name="_Recursive">하위 폴더 까지 더 볼 것인지?</param>
+	void AllFileRecursive(
+		const std::string_view _Path, 
+		std::list<UEngineFile>& _Result, 
+		std::vector<std::string> _Ext = std::vector<std::string>(), 
+		bool _Recursive = false);
 };
