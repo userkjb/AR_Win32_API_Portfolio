2c8f41cfffd50194cfcae74fde0acc96987caee4
KJB <kjb_jh@naver.com>
2024-02-06 16:04:09 +0900

    PathObject Class implementation.
    # PathObject Class 구현.

--- Mega_man_X4_WinAPI/EngineBase/PathObject.h
+++ Mega_man_X4_WinAPI/EngineBase/PathObject.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "NameObject.h"
+#include "EnginePath.h"
 
-class UPathObject
+/// <summary>
+/// 이름과 경로를 갖는 Object
+/// </summary>
+class UPathObject : public UNameObject
 {
 public :
 	// constructer destructer
@@ -15,9 +19,24 @@ public :
 	UPathObject& operator=(const UPathObject) = delete;
 	UPathObject& operator=(UPathObject&& _Other) noexcept = delete;
 
+	void SetPath(std::string_view _View)
+	{
+		Path = _View;
+	}
+
+	std::string GetPath()
+	{
+		return Path;
+	}
+
+	UEnginePath GetEnginePath()
+	{
+		return UEnginePath(std::filesystem::path(Path));
+	}
+
 protected :
 
 private :
-
+	std::string Path;
 };
 
