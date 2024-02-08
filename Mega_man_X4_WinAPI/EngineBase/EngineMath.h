13911a128c1321a7116fb6805c578459f9b69ebf
KJB <kjb_jh@naver.com>
2024-02-08 12:52:48 +0900

    Since it is inconvenient to always add static_cast for the data type Long to Float, we added the float4(long _X, long _Y) function to the EngineMath Class to make it easier to use.
    # 자료형 Long to Float  에 대해 매번 static_cast을 넣어주기 귀찮으니 이를 쉽게 사용하기 위해 EngineMath Class에  float4(long _X, long _Y) 함수 추가.

--- Mega_man_X4_WinAPI/EngineBase/EngineMath.h
+++ Mega_man_X4_WinAPI/EngineBase/EngineMath.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <cmath>
 
 struct float4
 {
+public :
 	static const float4 Zero;
 	static const float4 Left;
 	static const float4 Right;
@@ -13,6 +15,27 @@ struct float4
 	float Y; // 2D
 	float Z; // 3D
 	float W; // 3D
+
+	// 생성자를 한번 만들게 되면 리스트 이니셜라이저가 동작하지 않아서
+	// 개인이 생성하는 방식을 다 정의해야 합니다.
+	float4()
+		: X(0.0f), Y(0.0f), Z(0.0f), W(1.0f)
+	{
+
+	}
+	
+	float4(float _X, float _Y, float _Z, float _W)
+		: X(_X), Y(_Y), Z(_Z), W(_W)
+	{
+
+	}
+
+	// 사용하고자 하는 함수 추가.
+	float4(long _X, long _Y)
+		: X(static_cast<float>(_X)), Y(static_cast<float>(_Y)), Z(0.0f), W(1.0f)
+	{
+
+	}
 
 public:
 	float hX()
