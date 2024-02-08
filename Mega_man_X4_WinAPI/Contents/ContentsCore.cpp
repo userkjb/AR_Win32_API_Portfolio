988c3e2f1edaf183a26d5af658e774ce409b8899
KJB <kjb_jh@naver.com>
2024-02-08 14:03:32 +0900

    Coding to render images using EngineResourcesManager in ContentsCore.
    # ContentsCore에서 EngineResourcesManager를 이용해 이미지를 렌더할 수 있도록 코딩.

--- Mega_man_X4_WinAPI/Contents/ContentsCore.cpp
+++ Mega_man_X4_WinAPI/Contents/ContentsCore.cpp
@@ -1,6 +1,9 @@
 #include "ContentsCore.h"
 //#include "TitleLevel.h"
 #include "IntroLevel.h"
+#include <EngineBase/EngineDirectory.h>
+#include <EngineBase/EngineFile.h>
+#include <EngineCore/EngineResourcesManager.h>
 
 
 ContentsCore::ContentsCore()
@@ -17,6 +20,24 @@ ContentsCore::~ContentsCore()
 /// </summary>
 void ContentsCore::BeginPlay()
 {
+	// 부모 BeginPlay.
+	UEngineCore::BeginPlay();
+
+	// Resource 폴더 경로 설정.
+	UEngineDirectory NewDir;
+	NewDir.MoveParent();
+	NewDir.Move("ContentsResources");
+
+	std::list<UEngineFile> NewList = NewDir.AllFile({ ".png", ".bmp" }, true);
+
+	for (UEngineFile& File : NewList)
+	{
+		UEngineResourcesManager::GetInst().LoadImg(File.GetFullPath());
+	}
+
+
+
+
 	CreateLevel<UIntroLevel>("IntroLevel");
 
 	ChangeLevel("IntroLevel");
