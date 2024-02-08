b6e1ff70bda49af693fef37c166a2d136c7343e5
KJB <kjb_jh@naver.com>
2024-02-08 16:36:25 +0900

    Create ImageRenderer Class
    # ImageRenderer Class 추가.
    # Component

---
+++ Mega_man_X4_WinAPI/EngineCore/ImageRenderer.cpp
@@ -0,0 +1,9 @@
+#include "ImageRenderer.h"
+
+UImageRenderer::UImageRenderer()
+{
+}
+
+UImageRenderer::~UImageRenderer()
+{
+}
---
+++ Mega_man_X4_WinAPI/EngineCore/ImageRenderer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "SceneComponent.h"
+
+
+class UImageRenderer : public USceneComponent
+{
+public:
+	// constrcuter destructer
+	UImageRenderer();
+	~UImageRenderer();
+
+	// delete Function
+	UImageRenderer(const UImageRenderer& _Other) = delete;
+	UImageRenderer(UImageRenderer&& _Other) noexcept = delete;
+	UImageRenderer& operator=(const UImageRenderer& _Other) = delete;
+	UImageRenderer& operator=(UImageRenderer&& _Other) noexcept = delete;
+
+protected :
+
+
+private :
+
+};
