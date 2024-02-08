7b17cb78500142333d755e10edf36803926f01d5
KJB <kjb_jh@naver.com>
2024-02-08 16:25:31 +0900

    ActorComponent Class feature implementation.
    # ActorComponent Class 기능 구현.

--- Mega_man_X4_WinAPI/EngineCore/ActorComponent.h
+++ Mega_man_X4_WinAPI/EngineCore/ActorComponent.h
@@ -1,8 +1,13 @@
 #pragma once
 #include "TickObject.h"
 
+class AActor;
+
+// Actor에 대한 기능을 담당하는 Component
 class UActorComponent : public UTickObject
 {
+	friend AActor;
+
 public:
 	// constrcuter destructer
 	UActorComponent();
@@ -14,9 +19,22 @@ public:
 	UActorComponent& operator=(const UActorComponent& _Other) = delete;
 	UActorComponent& operator=(UActorComponent&& _Other) noexcept = delete;
 
+	// 어느 Actor에 속한 Component인지 확인.
+	AActor* GetOwner()
+	{
+		return Owner;
+	}
+
 protected :
 
 private :
+	// 내가 속한 Actor를 받음.
+	AActor* Owner = nullptr;
 
+	// 해당 Component가 속한 Actor를 설정.
+	void SetOwner(AActor* _Owner)
+	{
+		Owner = _Owner;
+	}
 };
 
