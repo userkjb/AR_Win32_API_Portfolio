22dad533c753e85581e5c05f5a57885ff6853f9a
KJB <kjb_jh@naver.com>
2024-02-13 16:06:32 +0900

    SetMapImage function implementation completed.
    # SetMapImage 함수 구현 완료.

--- Mega_man_X4_WinAPI/Contents/BackGroundMap.cpp
+++ Mega_man_X4_WinAPI/Contents/BackGroundMap.cpp
@@ -1,1 +1,28 @@
 #include "BackGroundMap.h"
+#include <EngineCore/EngineResourcesManager.h>
+
+ABackGroundMap::ABackGroundMap()
+{
+}
+
+ABackGroundMap::~ABackGroundMap()
+{
+}
+
+void ABackGroundMap::SetMapImage(std::string_view _MapImageName)
+{
+	Renderer->SetImage(_MapImageName);
+	UWindowImage* Image = Renderer->GetImage();
+	FVector ImageScale = Image->GetScale();
+	Renderer->SetTransform({ ImageScale.Half2D(), ImageScale });
+}
+
+void ABackGroundMap::BeginPlay()
+{
+	// Render 순서 설정
+	Renderer = CreateImageRenderer(0);
+}
+
+void ABackGroundMap::Tick(float _DeltaTime)
+{
+}
