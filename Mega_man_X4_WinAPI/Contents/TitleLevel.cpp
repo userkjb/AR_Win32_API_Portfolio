3eaec77f506cbf30f79451b0392060e586915eca
KJB <kjb_jh@naver.com>
2024-02-13 15:20:36 +0900

    Initialize BeginPlay, Tick, LevelStart, and LevelEnd functions.
    # BeginPlay, Tick, LevelStart, LevelEnd 함수 초기화.

--- Mega_man_X4_WinAPI/Contents/TitleLevel.cpp
+++ Mega_man_X4_WinAPI/Contents/TitleLevel.cpp
@@ -7,3 +7,25 @@ UTitleLevel::UTitleLevel()
 UTitleLevel::~UTitleLevel()
 {
 }
+
+void UTitleLevel::BeginPlay()
+{
+	ULevel::BeginPlay();
+
+
+}
+
+void UTitleLevel::Tick(float _DeltaTime)
+{
+	ULevel::Tick(_DeltaTime);
+}
+
+void UTitleLevel::LevelStart(ULevel* _Level)
+{
+	ULevel::LevelStart(_Level);
+}
+
+void UTitleLevel::LevelEnd(ULevel* _Level)
+{
+	ULevel::LevelEnd(_Level);
+}
--- Mega_man_X4_WinAPI/Contents/TitleLevel.h
+++ Mega_man_X4_WinAPI/Contents/TitleLevel.h
@@ -15,6 +15,10 @@ public :
 	UTitleLevel& operator=(UTitleLevel&& _Other) noexcept = delete;
 
 protected :
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+	void LevelStart(ULevel* _Level) override;
+	void LevelEnd(ULevel* _Level) override;
 
 private :
 
