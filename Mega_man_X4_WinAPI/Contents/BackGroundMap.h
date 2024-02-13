e1f89b46b069ef0a0fc370113ac82c02c76035c9
KJB <kjb_jh@naver.com>
2024-02-13 15:36:07 +0900

    Create BackGroundMap Actor.
    # 배경 이미지 전용 Actor 생성.

---
+++ Mega_man_X4_WinAPI/Contents/BackGroundMap.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+class ABackGroundMap : public AActor
+{
+public :
+	// constrcuter destructer
+	ABackGroundMap();
+	~ABackGroundMap();
+
+	// delete Function
+	ABackGroundMap(const ABackGroundMap& _Other) = delete;
+	ABackGroundMap(ABackGroundMap&& _Other) noexcept = delete;
+	ABackGroundMap& operator=(const ABackGroundMap& _Other) = delete;
+	ABackGroundMap& operator=(ABackGroundMap&& _Other) noexcept = delete;
+
+	/// <summary>
+	/// Map ¼³Á¤.
+	/// </summary>
+	/// <param name="_MapImageName"></param>
+	void SetMapImage(std::string_view _MapImageName);
+
+protected :
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private :
+	UImageRenderer* Renderer = nullptr;
+};
+
