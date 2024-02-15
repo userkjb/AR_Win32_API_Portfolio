d0060bc077b90fa1f19a1fb39279200fedbc3244
KJB <kjb_jh@naver.com>
2024-02-15 12:24:14 +0900

    Declaration of classes, functions, and variables required for animation.
    # Animation에 필요한 Class 와 함수, 변수 선언.

--- Mega_man_X4_WinAPI/EngineCore/ImageRenderer.h
+++ Mega_man_X4_WinAPI/EngineCore/ImageRenderer.h
@@ -3,6 +3,26 @@
 #include "SceneComponent.h"
 #include <EnginePlatform/WindowImage.h>
 
+/// <summary>
+/// Animation 기본 정의 Class
+/// </summary>
+class UAnimationInfo
+{
+public :
+	// 애니메이션을 구성하는 것은 이미지이고
+	// 이 이미지는 1장부터 시작한다.
+	UWindowImage* Image = nullptr;  // 애니메이션을 구성하는 이미지
+	std::string Name;				// 이미지 이름.
+	int CurFrame = 0;				// 애니메이션의 현재 프레임.
+	float CurTime = 0.0f;			// 애니메이션의 현재 시간.
+	bool Loop = false;				// 반복할 것인지?
+	bool IsEnd = false;				// 애니메이션의 상태.
+	std::vector<float> Times;
+	std::vector<int> Indexs;
+
+	int Update(float _DeltaTime);
+};
+
 class UImageRenderer : public USceneComponent
 {
 public:
@@ -45,6 +65,69 @@ public:
 		return Image;
 	}
 
+	// ==== Animation ====
+
+	/// <summary>
+	/// <para>원하는 이미지의 시작과 끝를 가지고 애니메이션을 만든다.</para>
+	/// <para>1, 3 을 넣으면 123 / 123 / 123 순으로 동작한다.</para>
+	/// </summary>
+	/// <param name="_AnimationName"></param>
+	/// <param name="_ImageName"></param>
+	/// <param name="_Start"></param>
+	/// <param name="_End"></param>
+	/// <param name="_Inter"></param>
+	/// <param name="_Loop"></param>
+	void CreateAnimation(
+		std::string_view _AnimationName,
+		std::string_view _ImageName,
+		int _Start,
+		int _End,
+		float _Inter,
+		bool _Loop = true
+	);
+
+	/// <summary>
+	/// <para>원하는 이미지의 위치 숫자를 Vector로 받아 애니메이션을 만든다.</para>
+	/// <para>{ 1, 2, 3 } 을 넣으면 123 / 123 로 동작한다.</para>
+	/// <para>{ 1, 2, 3, 2 } 을 넣으면 1232 / 1232 로 동작한다.</para>
+	/// </summary>
+	/// <param name="_AnimationName"></param>
+	/// <param name="_ImageName"></param>
+	/// <param name="_Indexs"></param>
+	/// <param name="_Inter"></param>
+	/// <param name="_Loop"></param>
+	void CreateAnimation(
+		std::string_view _AnimationName,
+		std::string_view _ImageName,
+		std::vector<int> _Indexs,
+		float _Inter,
+		bool _Loop = true
+	);
+
+	void ChangeAnimation(std::string_view _AnimationName, bool _IsForce = false, int _StartIndex = 0, float _Time = -1.0f);
+	void AnimationReset();
+
+	bool IsCurAnimationEnd() const
+	{
+		return CurAnimation->IsEnd;
+	}
+
+	int GetCurAnimationFrame() const
+	{
+		return CurAnimation->CurFrame;
+	}
+
+	int GetCurAnimationImageFrame() const
+	{
+		const std::vector<int>& Indexs = CurAnimation->Indexs;
+		return Indexs[CurAnimation->CurFrame];
+	}
+
+	float GetCurAnimationTime() const
+	{
+		return CurAnimation->CurTime;
+	}
+
 protected :
 	/// <summary>
 	/// USceneComponent
@@ -58,4 +141,7 @@ private :
 	FTransform ImageCuttingTransform; // 이미지에서 자를 위치. / Set
 	Color8Bit TransColor; // 자를 색상. / Set
 
+	// === Animation ===
+	std::map<std::string, UAnimationInfo> AnimationInfos;
+	UAnimationInfo* CurAnimation = nullptr;
 };
