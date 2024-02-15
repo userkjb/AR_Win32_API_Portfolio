2f746d5648e12d8e9d28b2a2674acdee810ba7bc
KJB <kjb_jh@naver.com>
2024-02-15 19:07:37 +0900

    Create and apply animation. Test complete.
    # Animation 만들고 적용하기. 테스트 완료.

--- Mega_man_X4_WinAPI/Contents/BackGroundMap.cpp
+++ Mega_man_X4_WinAPI/Contents/BackGroundMap.cpp
@@ -21,21 +21,28 @@ void ABackGroundMap::SetMapImage(std::string_view _MapImageName)
 	TitleRenderer->SetTransform({ ImageScale*1.25f, ImageScale * 2.5f });
 }
 
-void ABackGroundMap::SetMenuImage(std::string_view _LogoImageName)
-{
-	FVector ShowPosition = { 400, 400 };
-	MenuRenderer->SetImage(_LogoImageName);
-	UWindowImage* Image = MenuRenderer->GetImage();
-	FVector ImageScale = Image->GetScale();
-	MenuRenderer->SetTransform({ ShowPosition, {ImageScale.X / 3, ImageScale.Y} });
-}
-
 void ABackGroundMap::SetGameStartTextImage(std::string_view _ImageName)
 {
 	GameStartTextRenderer->SetImage(_ImageName);
 	UWindowImage* Image = GameStartTextRenderer->GetImage();
 	FVector ImageScale = Image->GetScale();
 	GameStartTextRenderer->SetTransform({ {(ImageScale.X * 2.0f) + 100.0f, (ImageScale.Y * 2.0f) + 350.0f}, ImageScale * 2.0f });
+}
+
+void ABackGroundMap::SetMenuAnimation(std::string_view _LogoImageName)
+{
+	FVector ShowPosition = { 400, 400 };
+
+	MenuRenderer->SetImage(_LogoImageName);
+	UWindowImage* Image = MenuRenderer->GetImage();
+	FVector ImageScale = Image->GetScale();
+	MenuRenderer->SetTransform({ ShowPosition, {ImageScale.X / 3, ImageScale.Y} });
+
+	MenuRenderer->CreateAnimation("Menu_1", _LogoImageName, 0, 0, 0.1f, false);
+	MenuRenderer->CreateAnimation("Menu_2", _LogoImageName, 1, 1, 0.1f, false);
+	MenuRenderer->CreateAnimation("Menu_3", _LogoImageName, 2, 2, 0.1f, false);
+	
+	MenuRenderer->ChangeAnimation("Menu_1");
 }
 
 void ABackGroundMap::BeginPlay()
@@ -45,18 +52,19 @@ void ABackGroundMap::BeginPlay()
 	// Render 순서 설정
 	TitleRenderer = CreateImageRenderer(0);
 	GameStartTextRenderer = CreateImageRenderer(1);
-	GameStartTextRenderer->SetActive(false); // test
 
 	MenuRenderer = CreateImageRenderer(1);
+	MenuRenderer->SetActive(false);
 }
 
 void ABackGroundMap::Tick(float _DeltaTime)
 {
 	AActor::Tick(_DeltaTime);
 
-	if (true == UEngineInput::IsDown('O')) // Enter 키 수정 예정.
+	if (true == UEngineInput::IsDown('O') && false == EnterKey) // Enter 키 수정 예정.
 	{
-		MenuChange();
+		GameStartTextRenderer->SetActive(false);
+		MenuRenderer->SetActive(true);
 		EnterKey = true;
 	}
 
@@ -64,20 +72,37 @@ void ABackGroundMap::Tick(float _DeltaTime)
 	{
 		if (true == UEngineInput::IsDown(38)) // Up
 		{
-
+			KeyCount--;
+			if (KeyCount <= 0)
+			{
+				KeyCount = 1;
+			}
 		}
 		else if (true == UEngineInput::IsDown(40)) // Down
 		{
-
+			KeyCount++;
+			if (KeyCount > 3)
+			{
+				KeyCount = 3;
+			}
 		}
-	}
-}
 
-void ABackGroundMap::MenuChange()
-{
-	if (true == GameStartTextRenderer->IsActive())
-	{
-		GameStartTextRenderer->SetActive(false);
-		MenuRenderer->SetActive(true);
+		switch (KeyCount)
+		{
+		case 1 :
+			MenuRenderer->ChangeAnimation("Menu_1");
+			EngineDebug::OutPutDebugText(std::to_string(KeyCount));
+			break;
+		case 2 :
+			MenuRenderer->ChangeAnimation("Menu_2");
+			EngineDebug::OutPutDebugText(std::to_string(KeyCount));
+			break;
+		case 3 :
+			MenuRenderer->ChangeAnimation("Menu_3");
+			EngineDebug::OutPutDebugText(std::to_string(KeyCount));
+			break;
+		default :
+			break;
+		}
 	}
 }
