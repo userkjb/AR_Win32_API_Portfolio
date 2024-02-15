6c726eaeb95bc8e9e615911aaa60ab8bfcb3dec6
KJB <kjb_jh@naver.com>
2024-02-15 21:29:23 +0900

    IntroStateMap Class implementation. Image settings and output Debug-only function applied.
    # IntroStateMap Class 구현.
    # 이미지 설정 및 출력
    # Debug 전용 함수 적용.

--- Mega_man_X4_WinAPI/Contents/IntroStageMap.cpp
+++ Mega_man_X4_WinAPI/Contents/IntroStageMap.cpp
@@ -1,4 +1,5 @@
 #include "IntroStageMap.h"
+#include <EngineCore/EngineCore.h>
 
 AIntroStageMap::AIntroStageMap()
 {
@@ -6,4 +7,56 @@ AIntroStageMap::AIntroStageMap()
 
 AIntroStageMap::~AIntroStageMap()
 {
+}
+
+void AIntroStageMap::SetMapImage(std::string_view _MapImageName)
+{
+	Renderer->SetImage(_MapImageName);
+	UWindowImage* Image = Renderer->GetImage();
+	FVector ImageScale = Image->GetScale();
+	Renderer->SetTransform({ ImageScale.Half2D(), ImageScale});
+}
+
+void AIntroStageMap::SetColMapImage(std::string_view _MapImageName)
+{
+	ColRenderer->SetImage(_MapImageName);
+	UWindowImage* Image = ColRenderer->GetImage();
+	FVector ImageScale = Image->GetScale();
+	ColRenderer->SetTransform({ ImageScale.Half2D(), ImageScale });
+	ColRenderer->SetActive(false);
+}
+
+void AIntroStageMap::SwitchDebug()
+{
+	if (true == Renderer->IsActive())
+	{
+		Renderer->SetActive(false);
+		ColRenderer->SetActive(true);
+	}
+	else
+	{
+		Renderer->SetActive(true);
+		ColRenderer->SetActive(false);
+	}
+}
+
+
+
+void AIntroStageMap::BeginPlay()
+{
+	AActor::BeginPlay();
+
+	Renderer = CreateImageRenderer(1);
+	ColRenderer = CreateImageRenderer(0);
+	ColRenderer->SetActive(false);
+}
+
+void AIntroStageMap::Tick(float _DeltaTime)
+{
+	AActor::Tick(_DeltaTime);
+
+	if (true == UEngineInput::IsDown('O'))
+	{
+		SwitchDebug();
+	}
 }
--- Mega_man_X4_WinAPI/Contents/IntroStageMap.h
+++ Mega_man_X4_WinAPI/Contents/IntroStageMap.h
@@ -14,9 +14,25 @@ public:
 	AIntroStageMap& operator=(const AIntroStageMap& _Other) = delete;
 	AIntroStageMap& operator=(AIntroStageMap&& _Other) noexcept = delete;
 
+	/// <summary>
+	/// Intro Stage 1 의 Map 설정.
+	/// </summary>
+	/// <param name="_MapImageName"></param>
+	void SetMapImage(std::string_view _MapImageName);
+	/// <summary>
+	/// Intro Stage 1의 Map의 Col
+	/// </summary>
+	/// <param name="_MapImageName"></param>
+	void SetColMapImage(std::string_view _MapImageName);
+
+	void SwitchDebug();
+
 protected :
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
 
 private :
-
+	UImageRenderer* Renderer = nullptr;		// Map 이미지
+	UImageRenderer* ColRenderer = nullptr;	// ColMap 이미지
 };
 
