89dd1606339a039000076a9c2475ce158078bb83
KJB <kjb_jh@naver.com>
2024-02-16 13:11:38 +0900

    MagentaA color declaration.
    # MagentaA 색상 구현.

--- Mega_man_X4_WinAPI/EngineBase/EngineMath.cpp
+++ Mega_man_X4_WinAPI/EngineBase/EngineMath.cpp
@@ -7,6 +7,7 @@ const float4 float4::Up    = {  0.0f, -1.0f, 0.0f, 0.0f };
 const float4 float4::Down  = {  0.0f,  1.0f, 0.0f, 0.0f };
 
 const Color8Bit Color8Bit::WhiteA = { 255, 255, 255, 0 };
+const Color8Bit Color8Bit::MagentaA = { 255, 0, 255, 0 };
 
 EngineMath::EngineMath()
 {
