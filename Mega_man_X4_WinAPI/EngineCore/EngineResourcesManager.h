8453c63be9d0ba35419821bec23ba0810a7a612f
KJB <kjb_jh@naver.com>
2024-02-16 15:34:38 +0900

    Comment Update
    # 주석 Update

--- Mega_man_X4_WinAPI/EngineCore/EngineResourcesManager.h
+++ Mega_man_X4_WinAPI/EngineCore/EngineResourcesManager.h
@@ -48,8 +48,8 @@ public :
 	/// 이미지를 잘라서 사용할 때 사용하는 함수.
 	/// </summary>
 	/// <param name="_Name">이미지 이름</param>
-	/// <param name="_X">행</param>
-	/// <param name="_Y">렬</param>
+	/// <param name="_X">행 개수</param>
+	/// <param name="_Y">렬 개수</param>
 	void CuttingImage(std::string_view _Name, int _X, int _Y);
 
 private :
