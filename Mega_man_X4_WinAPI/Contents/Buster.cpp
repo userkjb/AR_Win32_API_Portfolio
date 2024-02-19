28b2df2f4ed09f9ec817fe83da11a99779c5abaa
KJB <kjb_jh@naver.com>
2024-02-19 12:48:48 +0900

    Created the left Buster animation.
    # 왼쪽 Buster 애니메이션 만듦.

--- Mega_man_X4_WinAPI/Contents/Buster.cpp
+++ Mega_man_X4_WinAPI/Contents/Buster.cpp
@@ -19,6 +19,7 @@ void ABuster::BeginPlay()
 	Renderer->SetTransform({ {0, 0}, {ImageScale.X, (ImageScale.Y) * 3} }); // test
 
 	Renderer->CreateAnimation("Buster_Default_Right", "x_Buster_Default_Right.png", 0, 4, 0.05f, true);
+	Renderer->CreateAnimation("Buster_Default_Left", "x_Buster_Default_Left.png", 0, 4, 0.05f, true);
 
 	Renderer->ChangeAnimation("Buster_Default_Right");
 }
