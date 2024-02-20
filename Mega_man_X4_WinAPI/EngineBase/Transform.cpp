c742b7433a84377f9429439f53db93b42199b039
KJB <kjb_jh@naver.com>
2024-02-20 16:30:15 +0900

    Collision function implementation.
    # Collision 함수 구현.

--- Mega_man_X4_WinAPI/EngineBase/Transform.cpp
+++ Mega_man_X4_WinAPI/EngineBase/Transform.cpp
@@ -1,4 +1,5 @@
 #include "Transform.h"
+#include "EngineDebug.h"
 
 bool (*FTransform::CollisionFunction[static_cast<int>(ECollisionType::Max)][static_cast<int>(ECollisionType::Max)])(const FTransform& _Left, const FTransform& _Right);
 
@@ -12,5 +13,10 @@ FTransform::~FTransform()
 
 bool FTransform::Collision(ECollisionType _ThisType, ECollisionType _OtherType, const FTransform& _Other)
 {
+	if (nullptr == CollisionFunction[static_cast<int>(_ThisType)][static_cast<int>(_OtherType)])
+	{
+		MsgBoxAssert("아직 충돌 함수를 만들지 않은 충돌입니다");
+	}
 
+	return CollisionFunction[static_cast<int>(_ThisType)][static_cast<int>(_OtherType)](*this, _Other);
 }
