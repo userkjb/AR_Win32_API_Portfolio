3877c4d93fccd3a4896dbf58849df4ec56daf83f
KJB <kjb_jh@naver.com>
2024-02-20 13:51:38 +0900

    Add Command
    # 주석 추가.

--- Mega_man_X4_WinAPI/Contents/ContentsCore.cpp
+++ Mega_man_X4_WinAPI/Contents/ContentsCore.cpp
@@ -23,7 +23,7 @@ void ContentsCore::BeginPlay()
 {
 	// Main Buffer Image 설정
 	//MainWindow.SetWindowScale({ 256 * 4, 240 * 4 });
-	MainWindow.SetWindowPosition({ 500, 100 }); // 출력 위치.
+	MainWindow.SetWindowPosition({ 500, 100 }); // 윈도우에서 출력 위치.
 	MainWindow.SetWindowScale({ 800, 600 }); // 해상도
 
 	// 부모 BeginPlay.
