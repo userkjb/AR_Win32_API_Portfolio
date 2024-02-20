aca5dc3f4030e96c50e1dd7d39bacf267ab98283
KJB <kjb_jh@naver.com>
2024-02-20 16:04:32 +0900

    Added SetOrder function in Collision.
    # Collision에서 SetOrder 함수 추가.

--- Mega_man_X4_WinAPI/EngineCore/Collision.cpp
+++ Mega_man_X4_WinAPI/EngineCore/Collision.cpp
@@ -1,4 +1,6 @@
 #include "Collision.h"
+#include "Level.h"
+#include "Actor.h"
 
 UCollision::UCollision()
 {
@@ -6,4 +8,26 @@ UCollision::UCollision()
 
 UCollision::~UCollision()
 {
+}
+
+void UCollision::SetOrder(int _Order)
+{
+	AActor* Owner = GetOwner();
+	ULevel* Level = Owner->GetWorld();
+
+	std::map<int, std::list<UCollision*>>& Collisions = Level->Collisions;
+
+	// 내 오더가 바뀌기 전에 내가 어떤 그룹에 속해 있다면,
+	// 그 그룹에서 빠져나온다.
+	Collisions[GetOrder()].remove(this);
+
+	// 내 오더 바꾸기
+	UTickObject::SetOrder(_Order);
+
+	Collisions[GetOrder()].push_back(this);
+}
+
+void UCollision::BeginPlay()
+{
+	USceneComponent::BeginPlay();
 }
--- Mega_man_X4_WinAPI/EngineCore/Collision.h
+++ Mega_man_X4_WinAPI/EngineCore/Collision.h
@@ -12,5 +12,16 @@ class UCollision : public USceneComponent
 	UCollision(UCollision&& _Other) noexcept = delete;
 	UCollision& operator=(const UCollision& _Other) = delete;
 	UCollision& operator=(UCollision&& _Other) noexcept = delete;
-};
 
+	/// <summary>
+	/// 내가 속한 Level 과 Actor 설정.
+	/// </summary>
+	/// <param name="_Order"></param>
+	void SetOrder(int _Order) override;
+
+protected :
+	void BeginPlay() override;
+
+private :
+
+};
