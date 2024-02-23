6bb06863c9bd82e30d292b876e025875efbb1615
KJB <kjb_jh@naver.com>
2024-02-23 13:04:39 +0900

    Apply Buster state, declare functions according to state.
    # Buster 상태 적용, 상태에 따른 함수들 선언.

--- Mega_man_X4_WinAPI/Contents/Buster.h
+++ Mega_man_X4_WinAPI/Contents/Buster.h
@@ -21,6 +21,11 @@ public :
 		Dir = _Dir;
 	}
 
+	void SetBusterState(EBusterState _State)
+	{
+		BusterState = _State;
+	}
+
 protected :
 	void BeginPlay() override;
 	void Tick(float _DeltaTime) override;
@@ -29,19 +34,38 @@ protected :
 
 	void StateUpdate(float _DeltaTime);
 
-	// 상태 시작 함수.
+	// 상태 시작 함수. Begin
 	void DefaultBusterStart();
+	void MiddleChargeStart();
+	void PullchargeStart();
+	void BusterCrashStart();
+	void BusterEndStart();
 
 
 	// Buster 상태.
-	EBusterState State = EBusterState::CreateBuster;
+	EBusterState BusterState = EBusterState::DefaultCharge;
 
 	// Render 순서.
 	ERenderOrder Order = ERenderOrder::None;
 
 private :
-	FVector Dir = FVector::Zero; // 날아가는 방향
-	float BusterSpeed = 1.0f;	 // 발사 속도
+
+	// 상태 진행 함수.
+	void DefaultBuster(float _DeltaTime);
+	void MiddleCharge(float _DeltaTime);
+	void PullCharge(float _DeltaTime);
+	void BusterCrash(float _DeltaTime);
+	void BusterEnd(float _DeltaTime);
+
+
+
+	//=============================================
+
+	UImageRenderer* Renderer = nullptr;
 	UCollision* BusterCollision = nullptr;
+	FVector Dir = FVector::Zero; // 날아가는 방향
+	const float BusterSpeed = 1.0f;	 // 발사 속도
+
+	float BusterLifeTime = 0.0f;
 };
 
