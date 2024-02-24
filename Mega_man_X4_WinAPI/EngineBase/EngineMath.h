6b6f999614cc16db7e61ba1d85de34d5af24748b
KJB <kjb_jh@naver.com>
2024-02-24 21:47:55 +0900

    Implementation of the math needed for Lerp movement.
    # Lerp 이동에 필요한 수학 구현.

--- Mega_man_X4_WinAPI/EngineBase/EngineMath.h
+++ Mega_man_X4_WinAPI/EngineBase/EngineMath.h
@@ -50,6 +50,28 @@ public :
 	}
 	
 public:
+
+	static float4 LerpClamp(float4 p1, float4 p2, float d1)
+	{
+		if (0.0f >= d1)
+		{
+			d1 = 0.0f;
+		}
+
+		if (1.0f <= d1)
+		{
+			d1 = 1.0f;
+		}
+
+		return Lerp(p1, p2, d1);
+	}
+
+	// p1 p2          d1의 비율로 간다.
+	static float4 Lerp(float4 p1, float4 p2, float d1) 
+	{
+		return (p1 * (1.0f - d1)) + (p2 * d1);
+	}
+
 	/// <summary>
 	/// sqrtf = 제곱근 리턴.
 	/// </summary>
