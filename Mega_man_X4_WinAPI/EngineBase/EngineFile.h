ddb6490b17b2377b7a3e5ba1b5dd4cfeb2f2eb66
KJB <kjb_jh@naver.com>
2024-02-28 23:12:24 +0900

    Added EngineFile function.
    # EngineFile 기능 추가.

--- Mega_man_X4_WinAPI/EngineBase/EngineFile.h
+++ Mega_man_X4_WinAPI/EngineBase/EngineFile.h
@@ -1,5 +1,24 @@
 #pragma once
 #include "EnginePath.h"
+#include "EngineSerializer.h"
+
+/// <summary>
+/// Open Mode
+/// </summary>
+enum class IOOpenMode
+{
+	Write,
+	Read,
+};
+
+/// <summary>
+/// Data Type
+/// </summary>
+enum class IODataType
+{
+	Binary,
+	Text,
+};
 
 class UEngineFile : public UEnginePath
 {
@@ -7,4 +26,14 @@ public :
 	UEngineFile();
 	UEngineFile(std::filesystem::path _Path);
 	~UEngineFile();
+
+	void Open(IOOpenMode _OpenType, IODataType _DataType);
+	void Save(UEngineSerializer& _Data);
+	void Close();
+
+protected :
+
+private :
+	FILE* FileHandle = nullptr;
+
 };
