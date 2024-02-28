597dc3733f0391fefcffd200eb8ae493591e6924
KJB <kjb_jh@naver.com>
2024-02-28 23:15:34 +0900

    Added EngineFile function.
    # EngineFile 기능 추가.

--- Mega_man_X4_WinAPI/EngineBase/EngineFile.cpp
+++ Mega_man_X4_WinAPI/EngineBase/EngineFile.cpp
@@ -1,4 +1,7 @@
 #include "EngineFile.h"
+#include "EngineDebug.h"
+#include "EngineSerializer.h" //
+
 
 UEngineFile::UEngineFile()
 {
@@ -11,4 +14,59 @@ UEngineFile::UEngineFile(std::filesystem::path _Path)
 
 UEngineFile::~UEngineFile()
 {
+	Close();
+}
+
+void UEngineFile::Open(IOOpenMode _OpenType, IODataType _DataType)
+{
+	std::string Path = GetFullPath();
+
+	std::string Mode;
+
+	switch (_OpenType)
+	{
+	case IOOpenMode::Write:
+		Mode += "w";
+		break;
+	case IOOpenMode::Read:
+		Mode += "r";
+		break;
+	default:
+		break;
+	}
+
+	switch (_DataType)
+	{
+	case IODataType::Binary:
+		Mode += "b";
+		break;
+	case IODataType::Text:
+		Mode += "t";
+		break;
+	default:
+		break;
+	}
+
+	fopen_s(&FileHandle, Path.c_str(), Mode.c_str());
+
+	if (nullptr == FileHandle)
+	{
+		MsgBoxAssert("파일 오픈에 실패했습니다" + Path);
+	}
+}
+
+void UEngineFile::Save(UEngineSerializer& _Data)
+{
+	std::vector<char>& SaveData = _Data.Data;
+
+	char* StartPtr = &SaveData[0];
+	fwrite(StartPtr, SaveData.size(), 1, FileHandle);
+}
+
+void UEngineFile::Close()
+{
+	if (nullptr != FileHandle)
+	{
+		fclose(FileHandle);
+	}
 }
