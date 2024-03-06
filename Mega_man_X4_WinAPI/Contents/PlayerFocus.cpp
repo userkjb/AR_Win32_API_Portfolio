ca835d5ed08e7b1741e2f8855614492bb3758a75
KJB <kjb_jh@naver.com>
2024-03-06 16:46:44 +0900

    Write a focus function.
    # 포커스 기능 작성.

--- Mega_man_X4_WinAPI/Contents/PlayerFocus.cpp
+++ Mega_man_X4_WinAPI/Contents/PlayerFocus.cpp
@@ -1,4 +1,5 @@
 #include "PlayerFocus.h"
+#include "Egseu.h"
 
 APlayerFocus::APlayerFocus()
 {
@@ -26,13 +27,46 @@ void APlayerFocus::Tick(float _DeltaTime)
 
 void APlayerFocus::ForcusComponent()
 {
+	// Actor 의 위치.
 	FVector ActorPos = GetActorLocation();
+
+	FocusRender = CreateImageRenderer(static_cast<int>(ERenderOrder::MapObject));
+	FocusRender->SetImage("Focus.png");
+	FocusRender->AutoImageScale(2.0f);
+
+	FocusArrow = CreateImageRenderer(static_cast<int>(ERenderOrder::MapObject));
+	FocusArrow->SetImage("Focus.png");
+	FocusArrow->AutoImageScale(2.0f);
 
 	//RightUpRender = 
 	//LeftUpRender = 
 	//LeftDownRender = 
-	//HurryUpRender = 
-	//FocusRender = 
+	
+	HurryUpRender = CreateImageRenderer(static_cast<int>(ERenderOrder::MapObject));
+	HurryUpRender->SetImage("HurryUp.png");
+	HurryUpRender->AutoImageScale(2.0f);
+	HurryUpRender->SetPosition({ 0, 80 });
+
+	CreateAnimations();
+}
+
+void APlayerFocus::CreateAnimations()
+{
+	FocusRender->CreateAnimation("FocusStart", "Focus.png", 0, 2, 0.2f, false);
+	FocusRender->CreateAnimation("FocusLoop", "Focus.png", 3, 4, 0.2f, true);
+	FocusRender->CreateAnimation("FocusEnd", "Focus.png", { 2, 1, 0 }, 0.5f, false);
+	FocusRender->CreateAnimation("FocusIcon", "Focus.png", 0, 0, 0.5f, false);
+
+	FocusArrow->CreateAnimation("FocusLoop", "Focus.png", 5, 6, 0.3f, true);
+	FocusArrow->ActiveOff();
+
+	HurryUpRender->CreateAnimation("FocusLoop", "HurryUp.png", 0, 2, 0.2f, true);
+	HurryUpRender->ActiveOff();
+
+
+	FocusRender->ChangeAnimation("FocusStart");
+	FocusArrow->ChangeAnimation("FocusLoop");
+	HurryUpRender->ChangeAnimation("FocusLoop");
 }
 
 void APlayerFocus::StateChange(EFocusState _State)
@@ -50,8 +84,19 @@ void APlayerFocus::StateChange(EFocusState _State)
 		case EFocusState::Create:
 			CreateStart();
 			break;
+		case EFocusState::CreateLoop:
+			CreateLoopStart();
+			break;
 		case EFocusState::Run:
 			RunStart();
+			break;
+		case EFocusState::Rank:
+			break;
+		case EFocusState::RunUp:
+			RunUpStart();
+			break;
+		case EFocusState::RunRight:
+			RunRightStart();
 			break;
 		case EFocusState::Death:
 			DeathStart();
@@ -76,8 +121,20 @@ void APlayerFocus::StateUpdate(float _DeltaTime)
 	case EFocusState::Create:
 		Create(_DeltaTime);
 		break;
+	case EFocusState::CreateLoop:
+		CreateLoop(_DeltaTime);
+		break;
 	case EFocusState::Run:
 		Run(_DeltaTime);
+		break;
+	case EFocusState::Rank:
+		Rank(_DeltaTime);
+		break;
+	case EFocusState::RunUp:
+		RunUp(_DeltaTime);
+		break;
+	case EFocusState::RunRight:
+		RunRight(_DeltaTime);
 		break;
 	case EFocusState::Death:
 		Death(_DeltaTime);
@@ -90,36 +147,148 @@ void APlayerFocus::StateUpdate(float _DeltaTime)
 #pragma region CallCreate
 void APlayerFocus::CallCreate(float _DeltaTime)
 {
-	int a = 0;
+	StateChange(EFocusState::Create);
+	return;
 }
 #pragma endregion
 
 #pragma region Create
 void APlayerFocus::CreateStart()
 {
+	FocusRender->ChangeAnimation("FocusStart");
+	if (true == FocusArrow->IsActive())
+	{
+		FocusArrow->ActiveOff();
+	}
+	if (true == HurryUpRender->IsActive())
+	{
+		HurryUpRender->ActiveOff();
+	}
 }
 
 void APlayerFocus::Create(float _DeltaTime)
 {
+	if (true == FocusRender->IsCurAnimationEnd())
+	{
+		StateChange(EFocusState::CreateLoop);
+		return;
+	}
 }
+#pragma endregion
+
+#pragma region CreateLoop
+void APlayerFocus::CreateLoopStart()
+{
+	FocusRender->ChangeAnimation("FocusLoop");
+	if (false == FocusArrow->IsActive())
+	{
+		FocusArrow->ActiveOn();
+	}
+	if (false == HurryUpRender->IsActive())
+	{
+		HurryUpRender->ActiveOn();
+	}
+}
+
+void APlayerFocus::CreateLoop(float _DeltaTime)
+{
+	CreateDelay += _DeltaTime;
+	//UEngineDebug::OutPutDebugText(std::to_string(CreateDelay));
+	if (CreateDelay <= 1.5f)
+	{
+		return;
+	}
+	
+	StateChange(EFocusState::Run);
+	return;
+}
+
 #pragma endregion
 
 #pragma region Run
 void APlayerFocus::RunStart()
 {
+	CreateDelay = 0.0f;
+	HurryUpRender->ActiveOff();
 }
 
 void APlayerFocus::Run(float _DeltaTime)
 {
+	ForcusTime += _DeltaTime;
+	// Player를 반박자 뒤에 따라다녀야 함.
+	FVector PlayerPos = AEgseu::GetMainPlayer()->GetActorLocation();
+	PlayerPos.Y -= 58.0f;
+	FVector PlayerDir = PlayerPos - this->GetActorLocation();
+	PlayerDir.Normalize2D();
+
+	RunVector = PlayerDir * RunSpeed * _DeltaTime;
+	AddActorLocation(RunVector);
+	if (ForcusTime >= 10.0f)
+	{
+		HurryUpRender->ActiveOn();
+	}
 }
 #pragma endregion
+
+// Level에서 State를 바꾸면 이 쪽으로.
+void APlayerFocus::Rank(float _DeltaTime)
+{
+	StateChange(EFocusState::RunUp);
+	return;
+}
+
+#pragma region RunUp
+void APlayerFocus::RunUpStart()
+{
+	RunVector = FVector::Zero;
+	FocusRender->ChangeAnimation("FocusEnd");
+	HurryUpRender->ActiveOff();
+	FocusArrow->ActiveOff();
+	
+	// 끝나는 위치 = 내 위치 + 목표거리.
+	UpEndPos = GetActorLocation().iY() - UpTargetLen;
+}
+void APlayerFocus::RunUp(float _DeltaTime)
+{
+	RunVector = FVector::Up * RunSpeed * _DeltaTime;
+	AddActorLocation(RunVector);
+
+	// 움직이는 내 좌표와 끝나는 좌표랑 비교.
+	int CurPos = GetActorLocation().iY();
+	if (UpEndPos == CurPos)
+	{
+		StateChange(EFocusState::RunRight);
+		return;
+	}
+}
+#pragma endregion
+
+
+#pragma region RunRight
+void APlayerFocus::RunRightStart()
+{
+	RunVector = FVector::Zero;
+	FocusRender->ChangeAnimation("FocusIcon");
+}
+
+void APlayerFocus::RunRight(float _DeltaTime)
+{
+	RunVector = FVector::Right * RunSpeed * _DeltaTime;
+	AddActorLocation(RunVector);
+
+
+}
+#pragma endregion
+
 
 #pragma region Death
 void APlayerFocus::DeathStart()
 {
+	
 }
 
 void APlayerFocus::Death(float _DeltaTime)
 {
+	
 }
 #pragma endregion
--- Mega_man_X4_WinAPI/Contents/PlayerFocus.h
+++ Mega_man_X4_WinAPI/Contents/PlayerFocus.h
@@ -26,6 +26,7 @@ protected:
 
 private :
 	void ForcusComponent();
+	void CreateAnimations();
 
 	void StateChange(EFocusState _State);
 	void StateUpdate(float _DeltaTime);
@@ -36,19 +37,35 @@ private :
 	void CallCreate(float _DeltaTime);
 	void CreateStart();
 	void Create(float _DeltaTime);
+	void CreateLoopStart();
+	void CreateLoop(float _DeltaTime);
 	void RunStart();
 	void Run(float _DeltaTime);
+	void Rank(float _DeltaTime);
+	void RunUpStart();
+	void RunUp(float _DeltaTime);
+	void RunRightStart();
+	void RunRight(float _DeltaTime);
 	void DeathStart();
 	void Death(float _DeltaTime);
 
 	EFocusState State = EFocusState::None;
 
-	UImageRenderer* RightUpRender = nullptr;
-	UImageRenderer* LeftUpRender = nullptr;
-	UImageRenderer* LeftDownRender = nullptr;
-	UImageRenderer* HurryUpRender = nullptr;
 	UImageRenderer* FocusRender = nullptr;
+	UImageRenderer* FocusArrow = nullptr;
 
+	//UImageRenderer* RightUpRender = nullptr;
+	//UImageRenderer* LeftUpRender = nullptr;
+	//UImageRenderer* LeftDownRender = nullptr;
+
+	UImageRenderer* HurryUpRender = nullptr;
+
+	FVector RunVector = FVector::Zero;
+	float RunSpeed = 500.0f;
+	int UpTargetLen = 150;
+	int UpEndPos = 0;
+
+	float CreateDelay = 0.0f;
 	float ForcusTime = 0.0f;
 };
 
