739f422c63e5b9dabb666dfc999ecd1b52225cda
KJB <kjb_jh@naver.com>
2024-03-06 10:47:24 +0900

    Added and implemented two classes to use the sound library.
    # 사운드 라이브러리 를 사용할 수 있도록 Class 2개를 추가 및 구현.

---
+++ Mega_man_X4_WinAPI/EnginePlatform/EngineSound.h
@@ -0,0 +1,93 @@
+#pragma once
+#include <string>
+#include <string_view>
+#include <EngineBase\EnginePath.h>
+#include <map>
+
+#include "ThirdParty\FMOD\inc\fmod.hpp"
+
+class UEngineSoundPlayer
+{
+	friend class UEngineSound;
+
+public:
+	void On()
+	{
+		Control->setPaused(false);
+	}
+	void Off()
+	{
+		Control->setPaused(true);
+	}
+
+	void Loop(int Count = -1)
+	{
+		Control->setLoopCount(Count);
+	}
+
+	void Replay()
+	{
+		Control->setPosition(0, FMOD_TIMEUNIT_MS);
+	}
+
+	void OnOffSwitch()
+	{
+		bool Check = false;
+		Control->getPaused(&Check);
+
+		if (true == Check)
+		{
+			Control->setPaused(false);
+		}
+		else {
+			Control->setPaused(true);
+		}
+	}
+
+
+	void SetLoop(int Count);
+	void SetVolume(float _Volume);
+
+private:
+	// 이게 사운드 재생에 대한 권한 입니다.
+	FMOD::Channel* Control = nullptr;
+};
+
+class UEngineSound
+{
+	friend class ResControl;
+private:
+	static std::map<std::string, UEngineSound*> Resources;
+
+public:
+	// constrcuter destructer
+	UEngineSound();
+	~UEngineSound();
+
+	// delete Function
+	UEngineSound(const UEngineSound& _Other) = delete;
+	UEngineSound(UEngineSound&& _Other) noexcept = delete;
+	UEngineSound& operator=(const UEngineSound& _Other) = delete;
+	UEngineSound& operator=(UEngineSound&& _Other) noexcept = delete;
+
+	static UEngineSoundPlayer SoundPlay(std::string_view _Name);
+
+	static void Load(std::string_view _Path)
+	{
+		UEnginePath NewPath = UEnginePath(std::filesystem::path(_Path));
+		std::string FileName = NewPath.GetFileName();
+		return Load(_Path, FileName);
+	}
+
+	static void Load(std::string_view _Path, std::string_view _Name);
+	static void Update();
+
+protected:
+	void ResLoad(std::string_view _Path);
+
+private:
+	FMOD::Sound* SoundHandle = nullptr;
+
+	static void ResourcesRelease();
+};
+
