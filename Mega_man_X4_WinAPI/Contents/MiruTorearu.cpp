630ba2bc7afe57aa54e33369192824f410e75f85
KJB <kjb_jh@naver.com>
2024-03-06 11:26:49 +0900

    The final enemy spawn begins.
    # 마지막 적 생성 시작.

--- Mega_man_X4_WinAPI/Contents/MiruTorearu.cpp
+++ Mega_man_X4_WinAPI/Contents/MiruTorearu.cpp
@@ -1,1 +1,26 @@
 #include "MiruTorearu.h"
+
+AMiruTorearu::AMiruTorearu()
+{
+}
+
+AMiruTorearu::~AMiruTorearu()
+{
+}
+
+void AMiruTorearu::BeginPlay()
+{
+	AActor::BeginPlay();
+
+	BeginRender();
+}
+
+void AMiruTorearu::Tick(float _DeltaTime)
+{
+	AActor::Tick(_DeltaTime);
+}
+
+void AMiruTorearu::BeginRender()
+{
+
+}
--- Mega_man_X4_WinAPI/Contents/MiruTorearu.h
+++ Mega_man_X4_WinAPI/Contents/MiruTorearu.h
@@ -1,5 +1,31 @@
 #pragma once
-class AMiruTorearu
+#include <EngineCore/Actor.h>
+#include "ContentsGlobalData.h"
+
+class ABuster;
+
+class AMiruTorearu : public AActor
 {
+public:
+	// constrcuter destructer
+	AMiruTorearu();
+	~AMiruTorearu();
+
+	// delete Function
+	AMiruTorearu(const AMiruTorearu& _Other) = delete;
+	AMiruTorearu(AMiruTorearu&& _Other) noexcept = delete;
+	AMiruTorearu& operator=(const AMiruTorearu& _Other) = delete;
+	AMiruTorearu& operator=(AMiruTorearu&& _Other) noexcept = delete;
+
+protected :
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+private :
+
+	void BeginRender();
+
+
+	UImageRenderer* MiruTorearuRender = nullptr; // 본체 이미지
+	UCollision* MiruTorearuCollision = nullptr; // 본체 콜리전
 };
 
