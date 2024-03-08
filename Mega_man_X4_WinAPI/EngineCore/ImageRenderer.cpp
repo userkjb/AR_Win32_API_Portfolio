840f0c19d927f5ec648942c104b07aa14161ce5c
KJB <kjb_jh@naver.com>
2024-03-08 13:43:47 +0900

    Added right alignment of engine images.
    # 엔진 이미지 오른쪽 정렬 추가.

--- Mega_man_X4_WinAPI/EngineCore/ImageRenderer.cpp
+++ Mega_man_X4_WinAPI/EngineCore/ImageRenderer.cpp
@@ -232,6 +232,11 @@ void UImageRenderer::ImageRender(float _DeltaTime)
 		RendererTrans.AddPosition({ RendererTrans.GetScale().hX() , 0.0f });
 		break;
 	}
+	case EImageSortType::Right:
+	{
+		RendererTrans.AddPosition({ -RendererTrans.GetScale().hX() , 0.0f });
+		break;
+	}
 	default:
 		break;
 	}
--- Mega_man_X4_WinAPI/EngineCore/ImageRenderer.h
+++ Mega_man_X4_WinAPI/EngineCore/ImageRenderer.h
@@ -11,6 +11,7 @@ enum class EImageSortType
 {
 	Center,
 	Left,
+	Right,
 };
 
 /// <summary>
