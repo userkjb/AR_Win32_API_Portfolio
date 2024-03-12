e9391b08633fcef45bf562df93436b530f0229c3
KJB <kjb_jh@naver.com>
2024-03-12 11:06:11 +0900

    Added door animation. Collision creation and setup. Write a collision function.
    # 문 애니메이션 추가.
    # 콜리전 생성 및 설정.
    # 콜리전 함수 작성.

--- Mega_man_X4_WinAPI/Contents/CyberSpaceBossMap.cpp
+++ Mega_man_X4_WinAPI/Contents/CyberSpaceBossMap.cpp
@@ -20,6 +20,8 @@ void ACyberSpaceBossMap::BeginPlay()
 	MapColRenderer->SetActive(false);
 	BossDoor_1 = CreateImageRenderer(static_cast<int>(ERenderOrder::MapObject));
 	BossDoor_2 = CreateImageRenderer(static_cast<int>(ERenderOrder::MapObject));
+
+
 }
 
 void ACyberSpaceBossMap::Tick(float _DeltaTime)
@@ -28,6 +30,22 @@ void ACyberSpaceBossMap::Tick(float _DeltaTime)
 	if (true == UEngineInput::IsDown('O'))
 	{
 		SwitchDebug();
+	}
+
+	CollisionCheck(_DeltaTime);
+}
+
+void ACyberSpaceBossMap::SwitchDebug()
+{
+	if (true == MapRenderer->IsActive())
+	{
+		MapRenderer->SetActive(false);
+		MapColRenderer->SetActive(true);
+	}
+	else
+	{
+		MapRenderer->SetActive(true);
+		MapColRenderer->SetActive(false);
 	}
 }
 
@@ -51,7 +69,7 @@ void ACyberSpaceBossMap::SetMapColImage(std::string_view _MapColImageName)
 void ACyberSpaceBossMap::SetDoorImage(std::string_view _DoorImageName)
 {
 	BossDoor_1->SetImage(_DoorImageName);
-	UWindowImage* Image = BossDoor_1->GetImage();
+	//UWindowImage* Image = BossDoor_1->GetImage();
 	//ImageScale = Image->GetScale();
 	//BossDoor_1->SetTransform({ ImageScale.Half2D(), ImageScale });
 	//BossDoor_1->SetPosition({ 1080, 480 });
@@ -59,31 +77,54 @@ void ACyberSpaceBossMap::SetDoorImage(std::string_view _DoorImageName)
 	BossDoor_1->SetPosition(BossDoorPos_1);
 
 	BossDoor_2->SetImage(_DoorImageName);
-	UWindowImage* Image_2 = BossDoor_2->GetImage();
+	//UWindowImage* Image_2 = BossDoor_2->GetImage();
 	//ImageScale = Image_2->GetScale();
 	//BossDoor_2->SetTransform({ ImageScale.Half2D(), ImageScale });
 	//BossDoor_2->SetPosition({ 1840, 480 });
 	BossDoor_2->AutoImageScale(2.5f);
 	BossDoor_2->SetPosition(BossDoorPos_2);
 
+	BossDoor_Coll_1 = CreateCollision(ECollisionOrder::BossDoor);
+	BossDoor_Coll_1->SetColType(ECollisionType::Rect);
+	BossDoor_Coll_1->SetScale({75, 160});
+	BossDoor_Coll_1->SetPosition(BossDoor_1->GetPosition());
+
+	BossDoor_Coll_2 = CreateCollision(ECollisionOrder::BossDoor);
+	BossDoor_Coll_2->SetColType(ECollisionType::Rect);
+	BossDoor_Coll_2->SetScale({ 75, 160 });
+	BossDoor_Coll_2->SetPosition(BossDoor_2->GetPosition());
+
 	CreateDoorAni();
 }
 
 void ACyberSpaceBossMap::CreateDoorAni()
 {
+	BossDoor_1->CreateAnimation("Idle", "mmx4-bossdoor.png", 0, 0, 0.5f, true);
+	BossDoor_1->CreateAnimation("Open", "mmx4-bossdoor.png", 1, 12, 0.5f, false);
+	BossDoor_1->CreateAnimation("Close", "mmx4-bossdoor.png", { 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 0.5f, false);
 
+	BossDoor_2->CreateAnimation("Idle", "mmx4-bossdoor.png", 0, 0, 0.5f, true);
+	BossDoor_2->CreateAnimation("Open", "mmx4-bossdoor.png", 1, 12, 0.5f, false);
+	BossDoor_2->CreateAnimation("Close", "mmx4-bossdoor.png", { 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 0.5f, false);
+
+	BossDoor_1->ChangeAnimation("Idle");
+	BossDoor_2->ChangeAnimation("Idle");
 }
 
-void ACyberSpaceBossMap::SwitchDebug()
+
+
+void ACyberSpaceBossMap::CollisionCheck(float _DeltaTime)
 {
-	if (true == MapRenderer->IsActive())
+	std::vector<UCollision*> PlayerResult;
+	if (true == BossDoor_Coll_1->CollisionCheck(ECollisionOrder::Player, PlayerResult))
 	{
-		MapRenderer->SetActive(false);
-		MapColRenderer->SetActive(true);
+		int a = 0;
+		return;
 	}
-	else
+
+	if (true == BossDoor_Coll_2->CollisionCheck(ECollisionOrder::Player, PlayerResult))
 	{
-		MapRenderer->SetActive(true);
-		MapColRenderer->SetActive(false);
+		int a = 0;
+		return;
 	}
 }
