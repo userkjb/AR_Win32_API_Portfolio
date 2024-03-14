2217af4cb44ac1651fd68baad33fce9b344f4a00
KJB <kjb_jh@naver.com>
2024-03-14 18:11:21 +0900

    Created the missile class Base.
    # 미사일 클래스 Base 작성.

--- Mega_man_X4_WinAPI/Contents/FeatherMissile.cpp
+++ Mega_man_X4_WinAPI/Contents/FeatherMissile.cpp
@@ -1,1 +1,58 @@
 #include "FeatherMissile.h"
+#include <EngineCore/EngineCore.h> // Helper
+
+AFeatherMissile::AFeatherMissile()
+{
+}
+
+AFeatherMissile::~AFeatherMissile()
+{
+}
+
+void AFeatherMissile::BeginPlay()
+{
+	AActor::BeginPlay();
+
+	MissileRenderer = CreateImageRenderer(static_cast<int>(ERenderOrder::BossObject));
+	MissileRenderer->SetImage("missile.png");
+	MissileRenderer->AutoImageScale(2.0f);
+
+	MissileCollision = CreateCollision(ECollisionOrder::BossSkill);
+	MissileCollision->SetColType(ECollisionType::Rect);
+
+	StateChange(ECyberPeacockMissileState::None);
+}
+
+void AFeatherMissile::Tick(float _DeltaTime)
+{
+	AActor::Tick(_DeltaTime);
+
+	StateUpdate(_DeltaTime);
+}
+
+void AFeatherMissile::StateChange(ECyberPeacockMissileState _State)
+{
+	if (MillileState != _State)
+	{
+		switch (_State)
+		{
+		case ECyberPeacockMissileState::None:
+			break;
+		default:
+			break;
+		}
+	}
+
+	MillileState = _State;
+}
+
+void AFeatherMissile::StateUpdate(float _DeltaTime)
+{
+	switch (MillileState)
+	{
+	case ECyberPeacockMissileState::None:
+		break;
+	default:
+		break;
+	}
+}
--- Mega_man_X4_WinAPI/Contents/FeatherMissile.h
+++ Mega_man_X4_WinAPI/Contents/FeatherMissile.h
@@ -1,5 +1,35 @@
 #pragma once
-class AFeatherMissile
+#include <EngineCore/Actor.h>
+#include "ContentsGlobalData.h"
+
+class AFeatherMissile : public AActor
 {
+public :
+	// constrcuter destructer
+	AFeatherMissile();
+	~AFeatherMissile();
+
+	// delete Function
+	AFeatherMissile(const AFeatherMissile& _Other) = delete;
+	AFeatherMissile(AFeatherMissile&& _Other) noexcept = delete;
+	AFeatherMissile& operator=(const AFeatherMissile& _Other) = delete;
+	AFeatherMissile& operator=(AFeatherMissile&& _Other) noexcept = delete;
+
+protected :
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private :
+	void StateChange(ECyberPeacockMissileState _State);
+	void StateUpdate(float _DeltaTime);
+
+
+	UImageRenderer* MissileRenderer = nullptr;
+	UCollision* MissileCollision = nullptr;
+
+	ECyberPeacockMissileState MillileState = ECyberPeacockMissileState::None;
+
+	FVector MissileVector = FVector::Zero;
+	const float Speed = 1000.0f;
 };
 
