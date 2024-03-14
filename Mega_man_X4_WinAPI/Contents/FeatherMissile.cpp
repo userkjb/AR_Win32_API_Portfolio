9dc192290392e00ddaa5684cf85a810beb49db69
KJB <kjb_jh@naver.com>
2024-03-14 18:30:15 +0900

    Declaration and implementation of missile state functions.
    # 미사일 상태 함수들 선언 및 구현.

--- Mega_man_X4_WinAPI/Contents/FeatherMissile.cpp
+++ Mega_man_X4_WinAPI/Contents/FeatherMissile.cpp
@@ -20,6 +20,14 @@ void AFeatherMissile::BeginPlay()
 	MissileCollision = CreateCollision(ECollisionOrder::BossSkill);
 	MissileCollision->SetColType(ECollisionType::Rect);
 
+
+
+	// Player
+	if (Player == nullptr)
+	{
+		Player = AEgseu::GetMainPlayer();
+	}
+
 	StateChange(ECyberPeacockMissileState::None);
 }
 
@@ -37,6 +45,49 @@ void AFeatherMissile::StateChange(ECyberPeacockMissileState _State)
 		switch (_State)
 		{
 		case ECyberPeacockMissileState::None:
+			NoneStart();
+			break;
+		case ECyberPeacockMissileState::Create:
+			CreateStart();			
+			break;
+		case ECyberPeacockMissileState::Run_Up:
+			Run_UpStart();			
+			break;
+		case ECyberPeacockMissileState::Run_UpToRight:
+			Run_UpToRightStart();			
+			break;
+		case ECyberPeacockMissileState::Run_UpToLeft:
+			Run_UpToLeftStart();			
+			break;
+		case ECyberPeacockMissileState::Run_Right:
+			Run_RightStart();			
+			break;
+		case ECyberPeacockMissileState::Run_RightToUp:
+			Run_RightToUpStart();			
+			break;
+		case ECyberPeacockMissileState::Run_RightToDown:
+			Run_RightToDownStart();			
+			break;
+		case ECyberPeacockMissileState::Run_Left:
+			Run_LeftStart();			
+			break;
+		case ECyberPeacockMissileState::Run_LeftToUp:
+			Run_LeftToUpStart();			
+			break;
+		case ECyberPeacockMissileState::Run_LeftToDown:
+			Run_LeftToDownStart();			
+			break;
+		case ECyberPeacockMissileState::Run_Down:
+			Run_DownStart();			
+			break;
+		case ECyberPeacockMissileState::Run_DownToRight:
+			Run_DownToRightStart();			
+			break;
+		case ECyberPeacockMissileState::Run_DownToLeft:
+			Run_DownToLeftStart();			
+			break;
+		case ECyberPeacockMissileState::Death:
+			DeathStart();			
 			break;
 		default:
 			break;
@@ -51,8 +102,178 @@ void AFeatherMissile::StateUpdate(float _DeltaTime)
 	switch (MillileState)
 	{
 	case ECyberPeacockMissileState::None:
+		None(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Create:
+		Create(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_Up:
+		Run_Up(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_UpToRight:
+		Run_UpToRight(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_UpToLeft:
+		Run_UpToLeft(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_Right:
+		Run_Right(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_RightToUp:
+		Run_RightToUp(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_RightToDown:
+		Run_RightToDown(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_Left:
+		Run_Left(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_LeftToUp:
+		Run_LeftToUp(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_LeftToDown:
+		Run_LeftToDown(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_Down:
+		Run_Down(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_DownToRight:
+		Run_DownToRight(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Run_DownToLeft:
+		Run_DownToLeft(_DeltaTime);
+		break;
+	case ECyberPeacockMissileState::Death:
+		Death(_DeltaTime);
 		break;
 	default:
 		break;
 	}
+}
+
+void AFeatherMissile::NoneStart()
+{
+}
+
+void AFeatherMissile::None(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::CreateStart()
+{
+}
+
+void AFeatherMissile::Create(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_UpStart()
+{
+}
+
+void AFeatherMissile::Run_Up(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_UpToRightStart()
+{
+}
+
+void AFeatherMissile::Run_UpToRight(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_UpToLeftStart()
+{
+}
+
+void AFeatherMissile::Run_UpToLeft(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_RightStart()
+{
+}
+
+void AFeatherMissile::Run_Right(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_RightToUpStart()
+{
+}
+
+void AFeatherMissile::Run_RightToUp(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_RightToDownStart()
+{
+}
+
+void AFeatherMissile::Run_RightToDown(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_LeftStart()
+{
+}
+
+void AFeatherMissile::Run_Left(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_LeftToUpStart()
+{
+}
+
+void AFeatherMissile::Run_LeftToUp(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_LeftToDownStart()
+{
+}
+
+void AFeatherMissile::Run_LeftToDown(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_DownStart()
+{
+}
+
+void AFeatherMissile::Run_Down(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_DownToRightStart()
+{
+}
+
+void AFeatherMissile::Run_DownToRight(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::Run_DownToLeftStart()
+{
+}
+
+void AFeatherMissile::Run_DownToLeft(float _DeltaTime)
+{
+}
+
+void AFeatherMissile::DeathStart()
+{
+}
+
+void AFeatherMissile::Death(float _DeltaTime)
+{
+}
+
+
+
+
+void AFeatherMissile::CollisionCheck()
+{
 }
--- Mega_man_X4_WinAPI/Contents/FeatherMissile.h
+++ Mega_man_X4_WinAPI/Contents/FeatherMissile.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <EngineCore/Actor.h>
 #include "ContentsGlobalData.h"
+#include "Egseu.h"
+
+class AEgseu;
 
 class AFeatherMissile : public AActor
 {
@@ -23,13 +26,48 @@ private :
 	void StateChange(ECyberPeacockMissileState _State);
 	void StateUpdate(float _DeltaTime);
 
+	void NoneStart();
+	void None(float _DeltaTime);
+	void CreateStart();
+	void Create(float _DeltaTime);
+	void Run_UpStart();
+	void Run_Up(float _DeltaTime);
+	void Run_UpToRightStart();
+	void Run_UpToRight(float _DeltaTime);
+	void Run_UpToLeftStart();
+	void Run_UpToLeft(float _DeltaTime);
+	void Run_RightStart();
+	void Run_Right(float _DeltaTime);
+	void Run_RightToUpStart();
+	void Run_RightToUp(float _DeltaTime);
+	void Run_RightToDownStart();
+	void Run_RightToDown(float _DeltaTime);
+	void Run_LeftStart();
+	void Run_Left(float _DeltaTime);
+	void Run_LeftToUpStart();
+	void Run_LeftToUp(float _DeltaTime);
+	void Run_LeftToDownStart();
+	void Run_LeftToDown(float _DeltaTime);
+	void Run_DownStart();
+	void Run_Down(float _DeltaTime);
+	void Run_DownToRightStart();
+	void Run_DownToRight(float _DeltaTime);
+	void Run_DownToLeftStart();
+	void Run_DownToLeft(float _DeltaTime);
+	void DeathStart();
+	void Death(float _DeltaTime);
+
+	void CollisionCheck();
 
 	UImageRenderer* MissileRenderer = nullptr;
 	UCollision* MissileCollision = nullptr;
 
 	ECyberPeacockMissileState MillileState = ECyberPeacockMissileState::None;
 
+	float LifeTime = 0.0f;
 	FVector MissileVector = FVector::Zero;
 	const float Speed = 1000.0f;
+
+	AEgseu* Player = nullptr;
 };
 
